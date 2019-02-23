b63b8ed37d8b8b411f09a377daeaacf9d6e1a422
Your NamePaul <tagwoa305@yahoo.com>
2019-02-23 11:48:49 +0100

    Test

---
+++ SensorrService/Model/Device.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SensorrService.Model
+{
+    public class Device
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public int Temperature { get; set; }
+        public int HighVoltage { get; set; }
+        public int BatteryVoltage { get; set; }
+    }
+}
